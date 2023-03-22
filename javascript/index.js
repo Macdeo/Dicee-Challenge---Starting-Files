3529634571747ae2242ee0c42a7b37d5865d255a
Macdeo <akinbiyi.adeola@gmail.com>
2023-03-22 23:18:54 +0100

    modified:   index.html 	modified:   javascript/index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,22 @@
 
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 console.log(randomNumber1);
+console.log(randomNumber2);
+
+var randomDiceImag1 = "images/dice" + randomNumber1 + ".png";
+var randomDiceImag2 = 'images/dice' + randomNumber2 + '.png';
+
+var image1 = document.querySelectorAll('img')[0];
+var image2 = document.querySelectorAll('img')[1];
+
+image1.setAttribute('src', randomDiceImag1);
+image2.setAttribute('src', randomDiceImag2);
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector('h1').innerHTML = '🚩Player 1 Wins!';
+} else if(randomNumber1 < randomNumber2){
+    document.querySelector('h1').innerHTML = 'Player 2 Wins! 🚩';
+} else{
+    document.querySelector('h1').innerHTML = 'A draw!';
+}
